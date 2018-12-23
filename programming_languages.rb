979e528a722a1f283438e5e7ed77ca0f5f98d24c
Victoria B <viktorija.bourassa@gmail.com>
2018-12-23 17:16:05 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,11 @@
 def reformat_languages(languages)
-
-end
+ new_hash = {}
+      languages.each do |key, value|
+      value.each do |k, v|
+      v[:style] = [key]
+      new_hash[k] = v
+      end
+      new_hash[:javascript][:style] << :oo
+      end
+      puts new_hash
+      end
